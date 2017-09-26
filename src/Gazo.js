eb397db142dbccd3b1ec5c850afcd11d04e3d5a9
maangie <maangie906@gmail.com>
2017-09-26 15:35:21 +0900

    Gazo クラス追加

---
+++ src/Gazo.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import gazo from "./gazo.jpg";
+
+class Gazo extends Component {
+  render() {
+    return (
+      <div>
+        <img src={gazo} />
+      </div>
+    );
+  }
+}
+
+export default Gazo;
