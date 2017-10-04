0a2fd6750e2123a094c6f34546d51a32ff01130f
maangie <maangie906@gmail.com>
2017-10-04 18:55:58 +0900

    マウスで Gazo を動かす機能

--- src/Gazo.js
+++ src/Gazo.js
@@ -2,10 +2,57 @@ import React, { Component } from 'react';
 import gazo from "./gazo.jpg";
 
 class Gazo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      styles: {position: "relative", top: 0, left: 0},
+      position: {top: 0, left: 0},
+      onMouseDown: false
+    };
+  }
+
+  setPositionOfImg = (e) => {
+    this.setState({
+      styles: {
+        position: "relative",
+        top: e.clientY - this.state.position.top,
+        left: e.clientX - this.state.position.left
+      }
+    });
+  }
+
+  handleMouseDown = (e) => {
+    this.setState({ position: {
+      top: e.clientY - this.state.styles.top,
+      left: e.clientX - this.state.styles.left
+    }});
+
+    this.setState({isMouseDown: true});
+  }
+
+  handleMouseUp = (e) => {
+    this.setState({isMouseDown: false});
+    this.setPositionOfImg(e);
+  }
+
+  handleMouseMove = (e) => {
+    if (this.state.isMouseDown) {
+      this.setPositionOfImg(e);
+    }
+  }
+
   render() {
     return (
       <div>
-        <img src={gazo} />
+        <img
+          src={gazo}
+          alt={"cat"}
+          draggable={"false"}
+          style={this.state.styles}
+          onMouseDown={this.handleMouseDown}
+          onMouseMove={this.handleMouseMove}
+          onMouseUp={this.handleMouseUp}
+        />
       </div>
     );
   }
